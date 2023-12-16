3b483cd824618ec5380a28d3140f739dcf698a1e
khadimul <khadimulislam.dev@gmail.com>
2023-12-16 15:47:27 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/black_mamba/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.black_mamba
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/button/custom_button.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onPressed;
+
+  const CustomButton({super.key, required this.title, required this.onPressed});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 327,
+        height: 55,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              backgroundColor: Colors.black,
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 18),
+            ),
+            onPressed: onPressed,
+            child: Text(title),
+          ),
+        )
+    );
+  }
+}
