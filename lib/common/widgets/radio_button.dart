4f6a4923b162ab204c031c3545ecf8727c9db256
khadimul <khadimulislam.dev@gmail.com>
2023-12-19 13:00:47 +0600

    new design

--- lib/common/widgets/radio_button.dart
+++ lib/common/widgets/radio_button.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 enum GenderTypeEnum { Male, Female, Other }
 
-class RadioButton extends StatelessWidget {
+class RadioButton extends StatefulWidget {
   RadioButton({
     Key? key,
     required this.title,
     required this.value,
     required this.genderTypeEnum,
     required this.onChanged,
-}) : super (key: key);
-
+  }) : super(key: key);
 
   String? title;
   GenderTypeEnum value;
@@ -18,15 +18,23 @@ class RadioButton extends StatelessWidget {
   Function(GenderTypeEnum?)? onChanged;
 
   @override
+  State<RadioButton> createState() => _RadioButtonState();
+}
+
+class _RadioButtonState extends State<RadioButton> {
+  @override
   Widget build(BuildContext context) {
     return Expanded(
       child: RadioListTile<GenderTypeEnum>(
-        title: Text('${title}'),
-        value: GenderTypeEnum.Male,
-        groupValue: genderTypeEnum,
-        onChanged: onChanged,
+        title: Text(
+          '${widget.title}',
+          style: GoogleFonts.ubuntu(fontSize: 16, fontWeight: FontWeight.w700),
+        ),
+        value: widget.value,
+        groupValue: widget.genderTypeEnum,
+        onChanged: widget.onChanged,
         dense: true,
-        contentPadding: const EdgeInsets.all(0.0),
+        contentPadding: const EdgeInsets.all(1.0),
       ),
     );
   }
--- lib/screens/your_details_page.dart
+++ lib/screens/your_details_page.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
+import 'package:black_mamba/common/widgets/custom_image_picker.dart';
 import 'package:black_mamba/common/widgets/radio_button.dart';
+import 'package:black_mamba/utils/helpers/formatted_date.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 class YourDetailsPage extends StatefulWidget {
   const YourDetailsPage({super.key});
@@ -12,6 +17,17 @@ class YourDetailsPage extends StatefulWidget {
 class _YourDetailsPageState extends State<YourDetailsPage> {
   final _nameController = TextEditingController();
   GenderTypeEnum? genderTypeEnum;
+  File? image;
+  String titleText = 'Select Date of Birth';
+  final DateTime _dateTime = DateTime.now();
+
+  pickImage(ImageSource source) {
+    CustomImagePicker(source: source).pick(onPick: (File? image) {
+      setState(() {
+        this.image = image;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,104 +35,156 @@ class _YourDetailsPageState extends State<YourDetailsPage> {
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
           children: [
-            Text(
-              'Your Details',
-              style: GoogleFonts.ubuntu(
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              'This allows us to personalize your experience.',
-              style: GoogleFonts.ubuntu(
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-              ),
-            ),
-            const SizedBox(
-              height: 42,
-            ),
-            Center(
-              child: Column(
-                children: [
-                  const Icon(
-                    Icons.person,
-                    size: 100,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      _showImagePicker();
-                    },
-                    icon: const Icon(
-                      Icons.mode_edit_outline_rounded,
-                      size: 30,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Text(
-              'FULL NAME',
-              style: GoogleFonts.ubuntu(
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _nameController,
-              keyboardType: TextInputType.name,
-              decoration: const InputDecoration(
-                contentPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 1),
-                ),
-                hintText: 'Input your name',
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Text(
-              'Gender',
-              style: GoogleFonts.ubuntu(
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-              ),
-            ),
-            Row(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                RadioButton(
-                  title: GenderTypeEnum.Male.name,
-                  value: GenderTypeEnum.Male,
-                  genderTypeEnum: genderTypeEnum,
-                  onChanged: (value) {
-                    genderTypeEnum = value;
-                  },
+                Text(
+                  'Your Details',
+                  style: GoogleFonts.ubuntu(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-                RadioButton(
-                  title: GenderTypeEnum.Female.name,
-                  value: GenderTypeEnum.Female,
-                  genderTypeEnum: genderTypeEnum,
-                  onChanged: (value) {
-                    genderTypeEnum = value;
-                  },
+                const SizedBox(
+                  height: 20,
                 ),
-                RadioButton(
-                  title: GenderTypeEnum.Other.name,
-                  value: GenderTypeEnum.Other,
-                  genderTypeEnum: genderTypeEnum,
-                  onChanged: (value) {
-                    genderTypeEnum = value;
+                Text(
+                  'This allows us to personalize your experience.',
+                  style: GoogleFonts.ubuntu(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(
+                  height: 42,
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      image == null
+                          ? Container(
+                              width: 100.0,
+                              height: 100.0,
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.black),
+                              child: const Icon(
+                                Icons.person,
+                                size: 80,
+                                color: Colors.white,
+                              ),
+                            )
+                          : CircleAvatar(
+                              maxRadius: 80.0,
+                              backgroundImage: FileImage(image!),
+                            ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          _showImagePicker();
+                        },
+                        child: Image.asset(
+                          'assets/images/ic.png',
+                          width: 32,
+                          height: 32,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'FULL NAME',
+                  style: GoogleFonts.ubuntu(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _nameController,
+                  keyboardType: TextInputType.name,
+                  decoration: const InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1),
+                    ),
+                    hintText: 'Input your name',
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Gender',
+                  style: GoogleFonts.ubuntu(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+                Row(
+                  children: [
+                    RadioButton(
+                      title: GenderTypeEnum.Male.name,
+                      value: GenderTypeEnum.Male,
+                      genderTypeEnum: genderTypeEnum,
+                      onChanged: (value) {
+                        setState(() {
+                          genderTypeEnum = GenderTypeEnum.Male;
+                        });
+                      },
+                    ),
+                    RadioButton(
+                      title: GenderTypeEnum.Female.name,
+                      value: GenderTypeEnum.Female,
+                      genderTypeEnum: genderTypeEnum,
+                      onChanged: (value) {
+                        setState(() {
+                          genderTypeEnum = GenderTypeEnum.Female;
+                        });
+                      },
+                    ),
+                    RadioButton(
+                      title: GenderTypeEnum.Other.name,
+                      value: GenderTypeEnum.Other,
+                      genderTypeEnum: genderTypeEnum,
+                      onChanged: (value) {
+                        setState(() {
+                          genderTypeEnum = GenderTypeEnum.Other;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'DOB',
+                  style: GoogleFonts.ubuntu(
+                      fontWeight: FontWeight.w700, fontSize: 16),
+                ),
+                ListTile(
+                  shape: const OutlineInputBorder(
+                      borderSide: BorderSide(width: 1.0)),
+                  title: Text(titleText) ,
+                  onTap: () {
+                    setState(() {
+                     // titleText = getFormattedDate(_dateTime.toString(), pattern: 'dd/ MM/ yyyy');
+                    });
                   },
+                  trailing: IconButton(
+                    onPressed: () {
+                      _showDatePicker();
+                    },
+                    icon: const Icon(Icons.calendar_today_outlined),
+                  ),
                 ),
               ],
             ),
@@ -146,7 +214,10 @@ class _YourDetailsPageState extends State<YourDetailsPage> {
               children: [
                 Expanded(
                   child: InkWell(
-                    onTap: () {},
+                    onTap: () {
+                      pickImage(ImageSource.camera);
+                      Navigator.pop(context);
+                    },
                     child: const SizedBox(
                       child: Column(
                         children: [
@@ -162,7 +233,10 @@ class _YourDetailsPageState extends State<YourDetailsPage> {
                 ),
                 Expanded(
                   child: InkWell(
-                    onTap: () {},
+                    onTap: () {
+                      pickImage(ImageSource.gallery);
+                      Navigator.pop(context);
+                    },
                     child: const SizedBox(
                       child: Column(
                         children: [
@@ -181,6 +255,14 @@ class _YourDetailsPageState extends State<YourDetailsPage> {
           ),
         );
       },
+    );
+  }
+
+  void _showDatePicker() {
+    showDatePicker(
+      context: context,
+      firstDate: DateTime(1970),
+      lastDate: DateTime(2100),
     );
   }
 }
